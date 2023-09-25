2605caf890b27550bc1df93313d94f8f53297b88
Ikayani03 <Ikayanir@gmail.com>
2023-09-25 15:56:43 +0700

    penambahan controller kategori

---
+++ perpustakaan/app/Controllers/KategoriController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class KategoriController extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+
+    public function seni(){
+        
+    }
+    
+    public function olahraga(){
+        
+    }
+    
+    public function hiburan(){
+    
+    }
+}
