8b75d099ec0b35a61cd1ca8fc013545d96aa1f5f
Ikayani03 <Ikayanir@gmail.com>
2023-09-25 14:03:08 +0700

    Penambahan controller login

---
+++ perpustakaan/app/Controllers/LoginController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class LoginController extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+
+    public function Daftar(){
+        
+    }
+    
+    public function Username(){
+        
+    }
+
+    public function password(){
+        
+    }
+
+    public function Lupa_Password(){
+        
+    }
+}
