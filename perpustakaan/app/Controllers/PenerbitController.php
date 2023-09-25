50eb3ba9a1f260df2ff2c90c0940333e89fa8163
Ikayani03 <Ikayanir@gmail.com>
2023-09-25 16:03:59 +0700

    penambahan controller penerbit

---
+++ perpustakaan/app/Controllers/PenerbitController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class PenerbitController extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+    
+    public function judul(){
+        
+    }
+
+    public function penulis(){
+        
+    }
+    
+    public function tahun(){
+        
+    }
+}
